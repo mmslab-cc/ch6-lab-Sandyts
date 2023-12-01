e8abcb107823bd0d655f2ac566e3099ac4f2b6bf
Sandyts <st1005127@gmail.com>
2023-12-01 16:27:54 +0800

    s

--- ch6-lab1/include/Information.h
+++ ch6-lab1/include/Information.h
@@ -1,1 +1,6 @@
 #include <stdio.h>
+typedef struct _sPerson {
+	char name[8];
+	int gender;
+	int age;
+}Person;
--- ch6-lab1/source/main.c
+++ ch6-lab1/source/main.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "../include/Information.h"
 
 int main(void)
 {
+	Person person;
+	printf("Please input your name:  ");
+	scanf("%d   ", &person.name);
+
+	printf("Please input your gender<0 for woman, 1 for man>:  ");
+	scanf("%d   ", &person.gender);
+
+	printf("Please input your age:  ");
+	scanf("%d   ", &person.age);
+
+	printf("\nHi! %s\n", person.name);
+
+	if (person.gender == 0) {
+		printf("You ar %d-year-old woman.\n", person.age);
+	}
+	else {
+		printf("You ar %d-year-old man.\n", person.age);
+	}
+	system("pause");
 	return 0;
 }
 
